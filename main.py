fe69a6f09278c4c812e4b6adc7075ed101ddaf2a
yuyuanhang <yuyuanhang14@163.com>
2021-09-26 17:23:33 +1000

    synchronize

--- main.py
+++ main.py
@@ -8,7 +8,30 @@ if(__name__ == "__main__"):
     print('\tCSV File: {}'.format(args[0]))
 
     with open(args[0]) as csv_file:
-        spam_reader = csv.reader(csv_file, delimiter='|')
+        csv_reader = csv.reader(csv_file, delimiter='|')
 
-        header = next(spam_reader)
-        print(len(header))
+        header = next(csv_reader)
+        columns = len(header)
+
+        columnTypes = {}
+        for i in range(columns): columnTypes[i] = None
+
+        rows = 0
+        row_data = [[] for i in range(columns)]
+        for row in csv_reader:
+            rows += 1
+
+            if len(row) != columns:
+                continue
+
+            crt_column = 0
+            for item in row:
+                row_data[crt_column].append(item)
+                crt_column++
+
+        print('Column: {}'.format(columns))
+        print('Row: {}'.format(rows))
+
+        for i in range(10):
+            for j in range(columns):
+                print(row_data[j][i])
