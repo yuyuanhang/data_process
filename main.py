f67db8490a5d1097910646fdec2af30138d8feb8
yuyuanhang <yuyuanhang14@163.com>
2021-09-26 17:04:34 +1000

    synchronize

--- main.py
+++ main.py
@@ -8,5 +8,7 @@ if(__name__ == "__main__"):
     print('\tCSV File: {}'.format(args[0]))
 
     with open(args[0]) as csv_file:
-        spamreader = csv.reader(csv_file, delimiter='|')
-        print(len(spamreader))
+        spam_reader = csv.reader(csv_file, delimiter='|')
+
+        header = next(spam_reader)
+        print(len(header))
